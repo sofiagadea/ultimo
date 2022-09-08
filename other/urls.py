609a06f001f188adc8c62c8b485dc22f35c1558c
sofiagadea <slgadea@miuandes.cl>
2022-09-08 18:27:09 -0300

    segundo commit

--- other/urls.py
+++ other/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 # ######:8000/other
 urlpatterns = [
-    path("",views.simple_view)
+    path('',views.list_tasks,name='index'),
+    path('newTask/',views.create_task, name='new_task'),
+    path('addingDCL/',views.add_dcl, name='add_dcl'),
+    path('editTask/<int:id>/',views.edit_task, name='edit_task'),
 ]
--- other/views.py
+++ other/views.py
@@ -1,11 +1,47 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-def simple_view(request):
-    return HttpResponse("HOLA")
-
-
-
-
-
+from django.shortcuts import render, redirect
+from .models import Task
+from .forms import TaskForm
 # Create your views here.
+
+def list_tasks(request):
+    tasks = Task.objects.all()
+    context = {
+        'tasks': tasks
+    }
+    return render (request,'index.html',context)
+
+def create_task(request):
+    if request.method == 'GET':
+        task_form = TaskForm()
+        context = {
+            'form':task_form
+        }
+    else:
+        task_form = TaskForm(request.POST)
+        context = {
+            'form':task_form
+        }
+        if task_form.is_valid():
+            task_form.save()
+            return redirect('index')
+    return render (request,'create_task.html',context)
+
+def edit_task(request,id):
+    task = Task.objects.get(id = id)
+    if request.method == 'GET':
+        task_form = TaskForm(instance = task)
+        context = {
+            'form': task_form
+        }
+    else:
+        task_form = TaskForm(request.POST, instance = task)
+        context = {
+            'form':task_form
+        }
+        if task_form.is_valid():
+            task_form.save()
+            return redirect('index')
+    return render (request,'create_task.html',context)
+def add_dcl(request):
+    return render(request,'painterdcl.html')
+
